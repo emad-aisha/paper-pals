35931fde1e5020996318f11a361fa0b8b787b8c7
Mathurin.G <mathuringenty101@gmail.com>
2025-11-03 00:32:34 -0500

    EnemyAI :)
    Had to click three dots to make the enemy chase the player wow love this engine :)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent AgentAI;
+    [SerializeField] Renderer ModelColor;
+
+    [SerializeField] int HP;
+
+    Color ColorOriginally;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ColorOriginally = ModelColor.material.color;
+        //gameManager.instance.updateGameGoal();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //will look for player position and move towards it
+        AgentAI.SetDestination(gameManager.instance.player.transform.position); 
     }
 }
