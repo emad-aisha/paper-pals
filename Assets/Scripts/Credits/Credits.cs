ba57c6ceb93062b8b019837cfd7648f767dd20b5
Joelltk1 <Joelbadmanodjea@live.com>
2025-12-12 17:17:09 -0500

    half done credits

---
+++ Assets/Scripts/Credits/Credits.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public float scrollSpeed = 40f;
+
+    private RectTransform rectTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+}
