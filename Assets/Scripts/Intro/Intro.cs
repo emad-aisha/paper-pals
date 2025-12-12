0972d326693e0b557c917d4c5fc4510ba3c81883
cellus19 <93560559+cellus19@users.noreply.github.com>
2025-12-11 23:17:21 -0500

    Outro
    Outro yes

--- Assets/Scripts/Intro/Intro.cs
+++ Assets/Scripts/Intro/Intro.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Diagnostics;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,10 +12,19 @@ public class Intro : MonoBehaviour
     [SerializeField] RawImage StudioIcon;
     [SerializeField] RawImage GameIcon;
     [SerializeField] float AnimationTime = 1.5f;
+    [SerializeField] TextMeshProUGUI TYMessage;
     void Start()
     {
         Debug.Log("hello");
-        StartCoroutine(PlayIntro());   
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Intro"))
+        {
+            StartCoroutine(PlayIntro());
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Outro"))
+        {
+            StartCoroutine(PlayOutro());
+        }
     }
 
     IEnumerator PlayIntro()
@@ -41,6 +51,23 @@ public class Intro : MonoBehaviour
         SceneManager.LoadScene("Main Menu");
     }
 
+    IEnumerator PlayOutro()
+    {
+        SetAlpha(TYMessage, 0);
+        TYMessage.text = "Thanks for Playing";
+        yield return Fade(TYMessage, 0, 1, AnimationTime);
+        yield return new WaitForSeconds(1);
+        yield return Fade(TYMessage, 1, 0, AnimationTime);
+        yield return new WaitForSeconds(0.5f);
+        TYMessage.text = "From the Paper Pals Team";
+        yield return Fade(TYMessage, 0, 1, AnimationTime);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Fade(StudioIcon, 1, 0, AnimationTime));
+        StartCoroutine(Fade(GameIcon, 1, 0, AnimationTime));
+        StartCoroutine(Fade(TYMessage, 1, 0, AnimationTime));
+        yield return new WaitForSeconds(AnimationTime);
+    }
+
     IEnumerator Fade(RawImage Icon, float Start, float Finish, float Duration)
     {
         float Timer = 0;
@@ -55,10 +82,32 @@ public class Intro : MonoBehaviour
         SetAlpha(Icon, Finish);
     }
 
+    IEnumerator Fade(TextMeshProUGUI Msg, float Start, float Finish, float Duration)
+    {
+        float Timer = 0;
+
+        while (Timer < Duration)
+        {
+            float Alpha = Mathf.Lerp(Start, Finish, Timer / Duration);
+            SetAlpha(Msg, Alpha);
+            Timer += Time.deltaTime;
+            yield return null;
+        }
+        SetAlpha(Msg, Finish);
+    }
+
     void SetAlpha(RawImage Icon, float alpha)
     {
         Color color = Icon.color;
         color.a = alpha;
         Icon.color = color;
     }
+
+    void SetAlpha(TextMeshProUGUI Message, float alpha)
+    {
+        Color color = Message.color;
+        color.a = alpha;
+        Message.color = color;
+    }
+
 }
